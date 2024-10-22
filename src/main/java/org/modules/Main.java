6d77d7036c6e142f69843a8db30eda339e35ce50
UbaldoFlor <150185399+UbaldoFlor@users.noreply.github.com>
2024-10-22 09:52:58 -0600

    Adding tests to MergeSortAlgorithmsTest (#4)

--- src/main/java/org/modules/Main.java
+++ src/main/java/org/modules/Main.java
@@ -1,34 +1,6 @@
 package org.modules;
 
-import org.modules.module1.algorithms.StringAlgorithms;
-import org.modules.module1.algorithms.mergeSort.MergeSortAlgorithms;
-import org.modules.others.BalancedBracketsValidator;
-import org.modules.others.Contacts;
-
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
   public static void main(String[] args) {
-    List<Double> list = new ArrayList<>();
-    list.add(1.0);
-    list.add(10.0);
-    list.add(7.0);
-    list.add(2.0);
-    list.add(11.0);
-    list.add(Double.valueOf(5));
-    list.add(3.0);
-    list.add(6.0);
-    list.add((double) 0);
-    list.add((double) -1);
-    list.add(13.0);
-    list.add(10.0);
-    MergeSortAlgorithms mergeSortAlgorithms = new MergeSortAlgorithms();
-    mergeSortAlgorithms.mergeSortList(list);
-    for(Double number : list) {
-      System.out.println(number);
-    }
-    int number = 7 /2;
-    System.out.println(number);
   }
 }
--- src/test/java/org/modules/module1/algorithms/mergeSort/MergeSortAlgorithmsTest.java
+++ src/test/java/org/modules/module1/algorithms/mergeSort/MergeSortAlgorithmsTest.java
@@ -1,16 +1,29 @@
 package org.modules.module1.algorithms.mergeSort;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.modules.module1.datastructures.linkedList.ListNode;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class MergeSortAlgorithmsTest {
+
+  private static MergeSortAlgorithms mergeSortAlgorithms;
+
+  @BeforeAll
+  static void setUp() {
+    mergeSortAlgorithms = new MergeSortAlgorithms();
+  }
 
   @Test
   void mergeSort_sortsArraySuccessfully() {
     // Given
     int[] array = { 12, 11, 13, 5, 6, 7 };
-    MergeSortAlgorithms mergeSortAlgorithms = new MergeSortAlgorithms();
 
     // When
     mergeSortAlgorithms.mergeSort(array, 0, array.length - 1);
@@ -24,7 +37,6 @@ public class MergeSortAlgorithmsTest {
   void mergeSort_sortsArraySuccessfully2() {
     // Given
     int[] array = { 38, 27, 43, 3, 9, 82, 10 };
-    MergeSortAlgorithms mergeSortAlgorithms = new MergeSortAlgorithms();
 
     // When
     mergeSortAlgorithms.mergeSort(array, 0, array.length - 1);
@@ -38,7 +50,6 @@ public class MergeSortAlgorithmsTest {
   void mergeSort_doesNotSortArray_whenLengthEqualsOne() {
     // Given
     int[] array = { 1 };
-    MergeSortAlgorithms mergeSortAlgorithms = new MergeSortAlgorithms();
 
     // When
     mergeSortAlgorithms.mergeSort(array, 0, array.length - 1);
@@ -46,5 +57,80 @@ public class MergeSortAlgorithmsTest {
     // Then
     int[] expectedArray = { 1 };
     assertArrayEquals(expectedArray, array);
+  }
+
+  @Test
+  void sortList_returnsNull_whenHeadIsNull() {
+    // Given and when
+    ListNode result = mergeSortAlgorithms.sortList(null);
+
+    // Then
+    assertNull(result);
+  }
+
+  @Test
+  void sortList_returnsHead_whenHeadNextIsNull() {
+    // Given
+    ListNode head = new ListNode(1);
+
+    // When
+    ListNode result = mergeSortAlgorithms.sortList(head);
+
+    // Then
+    assertEquals(head, result);
+  }
+
+  @Test
+  void sortList_sortsListSuccessfully() {
+    // Given
+    ListNode head = new ListNode(4);
+    head.next = new ListNode(2);
+    head.next.next = new ListNode(1);
+    head.next.next.next = new ListNode(3);
+
+    // When
+    ListNode result = mergeSortAlgorithms.sortList(head);
+
+    // Then
+    assertEquals(1, result.val);
+    assertEquals(2, result.next.val);
+    assertEquals(3, result.next.next.val);
+    assertEquals(4, result.next.next.next.val);
+  }
+
+  @Test
+  void sortStringArray_sortsArraySuccessfully() {
+    // Given
+    String[] array = { "kiwi", "orange", "banana", "apple", "grape" };
+
+    // When
+    mergeSortAlgorithms.sortStringArray(array, 0, array.length - 1);
+
+    // Then
+    String[] expectedArray = { "apple", "banana", "grape", "kiwi", "orange" };
+    assertArrayEquals(expectedArray, array);
+  }
+
+  @Test
+  void mergeSortList_sortsListSuccessfully() {
+    // Given
+    List<Double> list = new ArrayList<>();
+    list.add(4.0);
+    list.add(2.0);
+    list.add(1.0);
+    list.add(3.0);
+    list.add(5.5);
+    List<Double> expectedList = new ArrayList<>();
+    expectedList.add(1.0);
+    expectedList.add(2.0);
+    expectedList.add(3.0);
+    expectedList.add(4.0);
+    expectedList.add(5.5);
+
+    // When
+    mergeSortAlgorithms.mergeSortList(list);
+
+    // Then
+    assertEquals(expectedList, list);
   }
 }
